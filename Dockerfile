4c42625c054697db36bd2c4635553e605cd7b041
KutsStas <kytc.stas@gmail.com>
2022-11-26 19:30:19 +0200

    -Test cloud db

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM adoptopenjdk/openjdk11
 WORKDIR /app
-COPY BikeID/0.0.1-SNAPSHOT.jar /app
+COPY target/bike-0.0.1-SNAPSHOT.jar /app
+
 EXPOSE 8080
+EXPOSE 5432
 #
 ENV JAVA_OPTS=""
-ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF-8 -jar 0.0.1-SNAPSHOT.jar" ]
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF-8 -jar bike-0.0.1-SNAPSHOT.jar" ]
---
+++ target/generated-sources/annotations/com/kytc/bikeID/mapper/BikeMapperImpl.java
@@ -0,0 +1,75 @@
+package com.kytc.bikeID.mapper;
+
+import com.kytc.bikeID.dto.BikeDto;
+import com.kytc.bikeID.entity.Bike;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-11-26T17:30:14+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class BikeMapperImpl implements BikeMapper {
+
+    @Override
+    public BikeDto toDto(Bike bike) {
+        if ( bike == null ) {
+            return null;
+        }
+
+        BikeDto bikeDto = new BikeDto();
+
+        bikeDto.setId( bike.getId() );
+        bikeDto.setBikeType( bike.getBikeType() );
+        bikeDto.setBikeBrand( bike.getBikeBrand() );
+        bikeDto.setBikeModel( bike.getBikeModel() );
+        bikeDto.setBikeColor( bike.getBikeColor() );
+        bikeDto.setFrameSize( bike.getFrameSize() );
+        bikeDto.setWheelSize( bike.getWheelSize() );
+        bikeDto.setFrameNumber( bike.getFrameNumber() );
+        bikeDto.setWarranty( bike.getWarranty() );
+        bikeDto.setLegalStatus( bike.getLegalStatus() );
+
+        return bikeDto;
+    }
+
+    @Override
+    public Bike toEntity(BikeDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Bike bike = new Bike();
+
+        bike.setId( dto.getId() );
+        bike.setBikeType( dto.getBikeType() );
+        bike.setBikeBrand( dto.getBikeBrand() );
+        bike.setBikeModel( dto.getBikeModel() );
+        bike.setBikeColor( dto.getBikeColor() );
+        bike.setFrameSize( dto.getFrameSize() );
+        bike.setWheelSize( dto.getWheelSize() );
+        bike.setFrameNumber( dto.getFrameNumber() );
+        bike.setWarranty( dto.getWarranty() );
+        bike.setLegalStatus( dto.getLegalStatus() );
+
+        return bike;
+    }
+
+    @Override
+    public List<BikeDto> toDtoList(List<Bike> bikes) {
+        if ( bikes == null ) {
+            return null;
+        }
+
+        List<BikeDto> list = new ArrayList<BikeDto>( bikes.size() );
+        for ( Bike bike : bikes ) {
+            list.add( toDto( bike ) );
+        }
+
+        return list;
+    }
+}
