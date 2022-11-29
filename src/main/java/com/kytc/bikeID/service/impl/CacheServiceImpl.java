e3ec1f1d65c9a312149795c80067f5b07ed36e82
KutsStas <kytc.stas@gmail.com>
2022-11-29 19:39:40 +0200

    - AWS update

--- src/main/java/com/kytc/bikeID/service/impl/CacheServiceImpl.java
+++ src/main/java/com/kytc/bikeID/service/impl/CacheServiceImpl.java
@@ -13,7 +13,7 @@ import redis.clients.jedis.JedisPool;
 @SuppressWarnings("PMD")
 public class CacheServiceImpl implements CacheService {
 
-    private final JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);
+    private final JedisPool jedisPool = new JedisPool("3.75.172.192", 6379);
 
     @Override
     public String addValue(String key, String value) {
