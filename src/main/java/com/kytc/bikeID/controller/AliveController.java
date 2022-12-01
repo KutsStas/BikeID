636cd71415180f133d582362653e41755c9573f0
KutsStas <kytc.stas@gmail.com>
2022-12-01 13:54:27 +0200

    - AWS update v0.3

--- src/main/java/com/kytc/bikeID/controller/AliveController.java
+++ src/main/java/com/kytc/bikeID/controller/AliveController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  * AliveController.
  */
 @RestController
-@RequestMapping("/alive")
+@RequestMapping("/")
 public class AliveController {
     /**
      * GetMapping.
