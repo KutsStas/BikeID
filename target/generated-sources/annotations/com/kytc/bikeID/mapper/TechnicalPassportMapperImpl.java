5ed9fa9334b542eb4177d46a11f6e8866bc6d291
KutsStas <kytc.stas@gmail.com>
2022-12-02 16:30:05 +0200

    - CI/CD all done test

--- target/generated-sources/annotations/com/kytc/bikeID/mapper/TechnicalPassportMapperImpl.java
+++ target/generated-sources/annotations/com/kytc/bikeID/mapper/TechnicalPassportMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-26T17:30:14+0200",
+    date = "2022-12-02T16:28:29+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/kytc/bikeID/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/kytc/bikeID/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-26T17:30:13+0200",
+    date = "2022-12-02T16:28:28+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
